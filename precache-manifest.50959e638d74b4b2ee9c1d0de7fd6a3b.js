52a38d484031b76c48d179fe29ad9c66c9b09ac0
Neelesh Sai <rneeleshsai@gmail.com>
2020-07-21 22:08:07 +0530

    Updates

---
+++ precache-manifest.50959e638d74b4b2ee9c1d0de7fd6a3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "671557cc4a50dd8056648a0fbb6140ae",
+    "url": "/random-quote/index.html"
+  },
+  {
+    "revision": "1b14052f094eec205992",
+    "url": "/random-quote/static/css/main.fdd936e7.chunk.css"
+  },
+  {
+    "revision": "521fd54ae9e9af0838e2",
+    "url": "/random-quote/static/js/2.b6515ce6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote/static/js/2.b6515ce6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b14052f094eec205992",
+    "url": "/random-quote/static/js/main.af6e3e03.chunk.js"
+  },
+  {
+    "revision": "281c3fdc18e22c6a427d",
+    "url": "/random-quote/static/js/runtime-main.ce5e683c.js"
+  }
+]);
